0841784714d0efb25b13ef0035298ef613c11380
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-10 00:14:54 +0900

    fix 投稿者以外もチャット履歴を見られるようにした

--- src/chat.js
+++ src/chat.js
@@ -1,5 +1,5 @@
 console.log('Chat Start!');
-import { get, onValue, push, ref, set } from "firebase/database";
+import { DataSnapshot, onChildAdded, onValue, push, ref, set } from "firebase/database";
 import { db } from "./index";
 
 class Chat {
@@ -9,6 +9,8 @@ class Chat {
         this.message = document.querySelector('#message');
         this.chatBord = document.querySelector('#chat-bord');
         this.sendButton = document.querySelector('#send-button');
+
+        this.postsRef = ref(db, 'posts/');
 
         // 送信ボタンに関数を設定
         this.sendButton.addEventListener('click', () => {
@@ -20,27 +22,25 @@ class Chat {
             console.log(this.username.value);
             console.log(this.message.value);
 
-            const postListRef = ref(db, 'posts/');
-            const newPostRef = push(postListRef);
+            const newPostRef = push(this.postsRef);
             set(newPostRef, {
                 username: this.username.value,
                 message: this.message.value,
             });
+        });
 
-            // チャット履歴を追記する
-            onValue(newPostRef, (snapshot) => {
-                const data = snapshot.val();
-                console.log(data);
+        // チャット履歴を追記する
+        onChildAdded(this.postsRef, snapshot => {
+            const data = snapshot.val();
 
-                const liusername = document.createElement('li');
-                const limessage = document.createElement('li');
+            const liusername = document.createElement('li');
+            const limessage = document.createElement('li');
 
-                this.chatBord.appendChild(liusername);
-                this.chatBord.appendChild(limessage);
+            this.chatBord.appendChild(liusername);
+            this.chatBord.appendChild(limessage);
 
-                liusername.innerText = data['username'];
-                limessage.innerText = data['message'];
-            });
+            liusername.innerText = data['username'];
+            limessage.innerText = data['message'];
         });
     }
 }
